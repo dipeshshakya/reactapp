2845ed8f6f64f1baa7b09e1ebe774487cd1fc834
dipesh shakya <dipeshshakya106@gmail.com>
2018-06-17 12:52:57 +0545

    clock app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// let name = 'DIPESH'
+// let obj = {
+// 	fname:'dipesh',
+// 	lname:'shakya'
 
-ReactDOM.render(
-  <h1>Hello, DIPESH!</h1>,
-  document.getElementById('root')
-);
+// }
+
+// const element =  <h1>Hello, {obj.fname} {obj.lname}!</h1>
+
+
+// function cartoon(name="dipesh"){
+// 	return <h1>cartoon{name}</h1>
+// }
+let clock =function(){
+	return <h2>{new Date().toLocaleTimeString()}</h2>
+}
+
+setInterval(function(){
+
+	ReactDOM.render(
+	  // element,
+	  // cartoon(),
+	  clock(),
+	  document.getElementById('root')
+	);
+
+
+},1000)
